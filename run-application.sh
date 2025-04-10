f6e22bc5cab8a401b98c9a2fa5e169133e4da9a1
Monstroe <colemontrose@gmail.com>
2025-04-10 19:29:46 -0400

    Latest

--- run-application.sh
+++ run-application.sh
@@ -1,3 +1,4 @@
+docker-compose down
 docker-compose -f smart-inspect-server/docker-compose.dev.yml down
 docker-compose -f smart-inspect-server/docker-compose.prod.yml down
 docker-compose up --build -d
